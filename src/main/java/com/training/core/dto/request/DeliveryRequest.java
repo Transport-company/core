f1eb6e1a8caa098a181a3ce12d34508272019dcc
Valery Emelyanov <emelva@rambler.ru>
2021-01-29 17:09:19 +0300

    Constraints for request dtos

--- src/main/java/com/training/core/dto/request/DeliveryRequest.java
+++ src/main/java/com/training/core/dto/request/DeliveryRequest.java
@@ -7,6 +7,7 @@ import lombok.Data;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 /**
@@ -19,67 +20,77 @@ public class DeliveryRequest {
     /**
      * Information about the client's desire to receive notifications
      */
-    @Schema(description = "to send or not send notifications to the client",
+    @Schema(description = "to send or not send notifications to the client", required = true,
             example = "true")
+    @NotNull(message = "The field is required")
     private final Boolean enabledNotifications;
 
     /**
      * Cost of delivery
      */
-    @Schema(description = "cost of the delivery",
+    @Schema(description = "cost of the delivery", required = true,
             example = "89.90")
+    @NotNull(message = "The field is required")
     private final BigDecimal sum;
 
     /**
      * Unique delivery number
      */
-    @Schema(description = "a generated unique string",
+    @Schema(description = "a generated unique string", required = true,
             example = "mdu4n286ndgTwGGspr8hw5D7sErW5Mgcz")
+    @NotNull(message = "The field is required")
     private final String trackingNumber;
 
     /**
      * Information about payment for cargo transit
      */
-    @Schema(description = "is the delivery paid for or not",
+    @Schema(description = "is the delivery paid for or not", required = true,
             example = "true")
+    @NotNull(message = "The field is required")
     private final Boolean isPaid;
 
     /**
      * Information about the delivery stage (cargo status)
      */
-    @Schema(description = "a delivery status",
+    @Schema(description = "a delivery status", required = true,
             example = "REGISTERED")
+    @NotNull(message = "The field is required")
     @Enumerated(EnumType.STRING)
     private final DeliveryStatus status;
 
     /**
      * Cargo information
      */
-    @Schema(description = "a cargo to send")
+    @Schema(description = "a cargo to send", required = true)
+    @NotNull(message = "The field is required")
     private final CargoRequest cargo;
 
     /**
      * Information about the sender
      */
-    @Schema(description = "the sender of the cargo")
+    @Schema(description = "the sender of the cargo", required = true)
+    @NotNull(message = "The field is required")
     private final ClientRequest sender;
 
     /**
      * Information about the recipient
      */
-    @Schema(description = "the recipient of the cargo")
+    @Schema(description = "the recipient of the cargo", required = true)
+    @NotNull(message = "The field is required")
     private final ClientRequest recipient;
 
     /**
      * Sending address
      */
-    @Schema(description = "an adress from which the cargo is sent")
+    @Schema(description = "an adress from which the cargo is sent", required = true)
+    @NotNull(message = "The field is required")
     private final AddressRequest sendingAddress;
 
     /**
      * Shipping address
      */
-    @Schema(description = "an adress of receipt of the cargo")
+    @Schema(description = "an adress of receipt of the cargo", required = true)
+    @NotNull(message = "The field is required")
     private final AddressRequest shippingAddress;
 
 }
--- src/main/java/com/training/core/dto/request/DeliveryStatusRequest.java
+++ src/main/java/com/training/core/dto/request/DeliveryStatusRequest.java
@@ -5,6 +5,8 @@ import com.training.core.model.DeliveryStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * An object for transferring data from a request to a controller about a new delivery status.
  */
@@ -14,8 +16,9 @@ public class DeliveryStatusRequest {
     /**
      * A new status.
      */
-    @Schema(description = "a new status",
+    @Schema(description = "a new status", required = true,
             example = "DELIVERED")
+    @NotNull(message = "The field is required")
     private final DeliveryStatus status;
 
     @JsonCreator
