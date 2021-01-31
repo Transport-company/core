ed0da18486e39ebcfe728f390e974266261ab0e7
Prokopyev Tikhon <Prokopyevt@gmail.com>
2021-01-31 21:55:59 +0400

    Generates a tracking number of a delivery

--- src/main/java/com/training/core/service/TrackingNumberGeneratingService.java
+++ src/main/java/com/training/core/service/TrackingNumberGeneratingService.java
@@ -1,26 +1,16 @@
 package com.training.core.service;
 
 
-import java.util.Random;
-import java.util.UUID;
-
 /**
  * Generates a tracking number of a delivery.
  */
 
 public interface TrackingNumberGeneratingService {
-    public static String generateStringTrackingNumber() {
 
-        String trackingNumber = UUID.randomUUID().toString(); // Generates a tracking number
-        Random randomTrackingNumber = new Random(); // Plug, track number existing in the database
-        boolean b = randomTrackingNumber.equals(trackingNumber);
-        if (b == true) {
-            String newTrackingNumber = UUID.randomUUID().toString();
-            return newTrackingNumber;
-        } else {
-            return trackingNumber;
-        }
-    }
+     String generateStringTrackingNumber();
+     /**
+      * method generates a track number
+      */
 }
 
 
---
+++ src/main/java/com/training/core/service/impl/TrackingNumberGeneratingServiceImpl.java
@@ -0,0 +1,27 @@
+package com.training.core.service.impl;
+
+import com.training.core.service.TrackingNumberGeneratingService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class TrackingNumberGeneratingServiceImpl implements TrackingNumberGeneratingService {
+
+    public String generateStringTrackingNumber() {
+        String trackingNumber = UUID.randomUUID().toString();
+        log.info("Generates a tracking number");
+        Random randomTrackingNumber = new Random();
+        boolean b = randomTrackingNumber.equals(trackingNumber);
+        if (b == true) {
+            String newTrackingNumber = UUID.randomUUID().toString();
+            return newTrackingNumber;
+        } else {
+            log.info("Track number recorded in base");
+            return trackingNumber;
+        }
+    }
+}
