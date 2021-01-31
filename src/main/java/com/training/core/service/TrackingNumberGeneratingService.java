4ba4e239b4e318d61475f6f9934a4d75a1d5520f
Prokopyev Tikhon <Prokopyevt@gmail.com>
2021-01-31 16:42:13 +0400

    Generates a tracking number of a delivery

--- src/main/java/com/training/core/service/TrackingNumberGeneratingService.java
+++ src/main/java/com/training/core/service/TrackingNumberGeneratingService.java
@@ -1,7 +1,26 @@
 package com.training.core.service;
 
+
+import java.util.Random;
+import java.util.UUID;
+
 /**
- * Generats a tracking number of a delivery.
+ * Generates a tracking number of a delivery.
  */
+
 public interface TrackingNumberGeneratingService {
+    public static String generateStringTrackingNumber() {
+
+        String trackingNumber = UUID.randomUUID().toString(); // Generates a tracking number
+        Random randomTrackingNumber = new Random(); // Plug, track number existing in the database
+        boolean b = randomTrackingNumber.equals(trackingNumber);
+        if (b == true) {
+            String newTrackingNumber = UUID.randomUUID().toString();
+            return newTrackingNumber;
+        } else {
+            return trackingNumber;
+        }
+    }
 }
+
+
