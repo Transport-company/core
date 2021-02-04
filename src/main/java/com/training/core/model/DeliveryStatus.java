dfda7823f631d72b7a771d6c024ac6d94ad63836
Gelnaz Abdreeva <gelnasotschek@mail.ru>
2021-02-04 22:29:31 +0400

    the changes affected the delivery statuses

--- src/main/java/com/training/core/model/DeliveryStatus.java
+++ src/main/java/com/training/core/model/DeliveryStatus.java
@@ -1,10 +1,43 @@
 package com.training.core.model;
 
 public enum DeliveryStatus {
+    /**
+     * Delivery record is open.
+     */
     REGISTERED,
+
+    /**
+     * Delivery is paid.
+     */
     PAID,
-    SENT,
-    COURIER_DELIVERY,
+
+    /**
+     * Cargo in transfer.
+     */
+    IN_TRANSFER,
+
+    /**
+     * Cargo is in the destination city. Forwarded to courier delivery.
+     */
+    ON_COURIER_DELIVERY,
+
+    /**
+     * The cargo is on the way back to the sender.
+     */
+    ON_RETURN,
+
+    /**
+     *  The cargo was delivered. Shipping is closed.
+     */
     DELIVERED,
-    RETURNED
+
+    /**
+     * The cargo was returned to the sender.
+     */
+    RETURNED,
+
+    /**
+     * The cargo was lost
+     */
+    LOST
 }
