0431f0dd5b8bf610c6b8ba6251e29f55251ce235
Valery Emelyanov <emelva@rambler.ru>
2021-02-08 10:08:13 +0300

    Delivery intagration test

--- src/test/java/com/training/core/controller/impl/DeliveryControllerImplTest.java
+++ src/test/java/com/training/core/controller/impl/DeliveryControllerImplTest.java
@@ -9,6 +9,7 @@ import com.training.core.dto.request.AddressRequest;
 import com.training.core.dto.request.CargoRequest;
 import com.training.core.dto.request.ClientRequest;
 import com.training.core.dto.request.DeliveryRequest;
+import com.training.core.dto.request.DeliveryStatusRequest;
 import com.training.core.dto.response.DeliveryPageResponse;
 import com.training.core.dto.response.DeliveryResponse;
 import com.training.core.model.DeliveryStatus;
@@ -36,8 +37,11 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -62,15 +66,21 @@ class DeliveryControllerImplTest {
     @Autowired
     private AddressRepository addressRepository;
 
-    private final ObjectMapper objectMapper = new ObjectMapper()
+    private static final ObjectMapper objectMapper = new ObjectMapper()
             .registerModule(new JavaTimeModule())
             .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
+    private static int deliveryCount = 0;
+    private static int deliveryCountPaid = 0;
+    private static int addressCount = 0;
+    private static int clientCount = 0;
+    private static Long deliveryId1 = null;
+
     @Test
     @Order(1)
     void create1() throws Exception {
-        DeliveryRequest request = getDelivery1();
+        DeliveryRequest request = getDelivery1(true);
 
         LocalDateTime start = LocalDateTime.now();
 
@@ -81,16 +91,22 @@ class DeliveryControllerImplTest {
                 .andExpect(status().isCreated())
                 .andReturn();
 
-        assertEquals(1, deliveryRepository.findAll().size());
-        assertEquals(1, cargoRepository.findAll().size());
-        assertEquals(2, clientRepository.findAll().size());
-        assertEquals(2, addressRepository.findAll().size());
+        deliveryCount++;
+        addressCount += 2;
+        clientCount += 2;
+
+        assertEquals(deliveryCount, deliveryRepository.findAll().size());
+        assertEquals(deliveryCount, cargoRepository.findAll().size());
+        assertEquals(clientCount, clientRepository.findAll().size());
+        assertEquals(addressCount, addressRepository.findAll().size());
 
         String content = result.getResponse().getContentAsString();
         DeliveryResponse response = objectMapper.readValue(content, DeliveryResponse.class);
 
         assertNotNull(response);
         assertNotNull(response.getId());
+
+        deliveryId1 = response.getId();
 
         assertEquals(request.getEnabledNotifications(), response.getEnabledNotifications());
         assertEquals(request.getSum(), response.getSum());
@@ -135,9 +151,9 @@ class DeliveryControllerImplTest {
                 && LocalDateTime.now().isAfter(response.getUpdated()));
     }
 
-    private DeliveryRequest getDelivery1() {
+    private DeliveryRequest getDelivery1(boolean enabledNotifications) {
         return DeliveryRequest.builder()
-                .enabledNotifications(true)
+                .enabledNotifications(enabledNotifications)
                 .sum(new BigDecimal("0.00"))
                 .isPaid(false)
                 .status(DeliveryStatus.REGISTERED)
@@ -203,6 +219,182 @@ class DeliveryControllerImplTest {
 
     @Test
     @Order(2)
+    void delete1() throws Exception {
+        mockMvc.perform(delete(Urls.Deliveries.FULL + "/" + deliveryId1))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        deliveryCount--;
+        deliveryId1 = null;
+
+        assertEquals(deliveryCount, deliveryRepository.findAll().size());
+        assertEquals(deliveryCount, cargoRepository.findAll().size());
+        assertEquals(clientCount, clientRepository.findAll().size());
+        assertEquals(addressCount, addressRepository.findAll().size());
+    }
+
+    @Test
+    @Order(3)
+    void create1Again() throws Exception {
+        DeliveryRequest request = getDelivery1(true);
+
+        MvcResult result = mockMvc.perform(post(Urls.Deliveries.FULL)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        deliveryCount++;
+
+        assertEquals(deliveryCount, deliveryRepository.findAll().size());
+        assertEquals(deliveryCount, cargoRepository.findAll().size());
+        assertEquals(2, clientRepository.findAll().size());
+        assertEquals(2, addressRepository.findAll().size());
+
+        String content = result.getResponse().getContentAsString();
+        DeliveryResponse response = objectMapper.readValue(content, DeliveryResponse.class);
+
+        assertNotNull(response);
+        assertNotNull(response.getId());
+
+        deliveryId1 = response.getId();
+    }
+
+    @Test
+    @Order(4)
+    void create2() throws Exception {
+        DeliveryRequest request = getDelivery2();
+
+        MvcResult result = mockMvc.perform(post(Urls.Deliveries.FULL)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        deliveryCount++;
+        addressCount++;
+        clientCount++;
+
+        assertEquals(deliveryCount, deliveryRepository.findAll().size());
+        assertEquals(deliveryCount, cargoRepository.findAll().size());
+        assertEquals(clientCount, clientRepository.findAll().size());
+        assertEquals(addressCount, addressRepository.findAll().size());
+
+        String content = result.getResponse().getContentAsString();
+        DeliveryResponse response = objectMapper.readValue(content, DeliveryResponse.class);
+
+        assertNotNull(response);
+        assertNotNull(response.getId());
+    }
+
+    private DeliveryRequest getDelivery2() {
+        return DeliveryRequest.builder()
+                .enabledNotifications(true)
+                .sum(new BigDecimal("0.00"))
+                .isPaid(false)
+                .status(DeliveryStatus.REGISTERED)
+                .cargo(getCargo2())
+                .sender(getSender1())
+                .recipient(getRecipient2())
+                .sendingAddress(getSendingAdress1())
+                .shippingAddress(getShippingAdress2())
+                .build();
+    }
+
+    private CargoRequest getCargo2() {
+        return CargoRequest.builder()
+                .weight(1.2f)
+                .declaredValue(new BigDecimal("1500.00"))
+                .length(25f)
+                .width(12f)
+                .height(10f)
+                .build();
+    }
+
+    private ClientRequest getRecipient2() {
+        return ClientRequest.builder()
+                .lastName("Cvetkov")
+                .firstName("Vitaliy")
+                .middleName("Sergeevich")
+                .birthday(LocalDate.of(1980, 4, 27))
+                .phoneNumber("89003450002")
+                .email("Cvetkov_VS@gmail.com")
+                .build();
+    }
+
+    private AddressRequest getShippingAdress2() {
+        return AddressRequest.builder()
+                .region("Kostromskaya oblast")
+                .city("Kostroma")
+                .street("Lesnaya")
+                .house("40")
+                .apartment("3")
+                .build();
+    }
+
+    @Test
+    @Order(5)
+    void update() throws Exception {
+        DeliveryRequest request = getDelivery1(false);
+
+        LocalDateTime start = LocalDateTime.now();
+
+        MvcResult result = mockMvc.perform(put(Urls.Deliveries.FULL + "/" + deliveryId1)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertEquals(deliveryCount, deliveryRepository.findAll().size());
+        assertEquals(deliveryCount, cargoRepository.findAll().size());
+        assertEquals(clientCount, clientRepository.findAll().size());
+        assertEquals(addressCount, addressRepository.findAll().size());
+
+        String content = result.getResponse().getContentAsString();
+        DeliveryResponse response = objectMapper.readValue(content, DeliveryResponse.class);
+
+        assertNotNull(response);
+        assertEquals(request.getEnabledNotifications(), response.getEnabledNotifications());
+
+        assertNotNull(response.getUpdated());
+        assertTrue(start.isBefore(response.getUpdated())
+                && LocalDateTime.now().isAfter(response.getUpdated()));
+    }
+
+    @Test
+    @Order(6)
+    void changeStatus() throws Exception {
+        DeliveryStatus status = DeliveryStatus.PAID;
+        DeliveryStatusRequest statusRequest = new DeliveryStatusRequest(status);
+
+        LocalDateTime start = LocalDateTime.now();
+
+        MvcResult result = mockMvc.perform(
+                patch(Urls.Deliveries.FULL + "/" + deliveryId1 + "/" + Urls.Deliveries.Status.PART)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(statusRequest)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        deliveryCountPaid++;
+
+        String content = result.getResponse().getContentAsString();
+        DeliveryResponse response = objectMapper.readValue(content, DeliveryResponse.class);
+
+        assertNotNull(response);
+        assertEquals(status, response.getStatus());
+
+        assertNotNull(response.getUpdated());
+        assertTrue(start.isBefore(response.getUpdated())
+                && LocalDateTime.now().isAfter(response.getUpdated()));
+    }
+
+    @Test
+    @Order(7)
     void getList() throws Exception {
         MvcResult result = mockMvc.perform(get(Urls.Deliveries.FULL))
                 .andDo(print())
@@ -214,30 +406,59 @@ class DeliveryControllerImplTest {
         DeliveryPageResponse response = objectMapper.readValue(content, DeliveryPageResponse.class);
 
         assertNotNull(response);
-        assertEquals(1, response.getTotalElements());
+        assertEquals(deliveryCount, response.getTotalElements());
 
         List<DeliveryResponse> list = response.getContent();
         assertNotNull(list);
-        assertEquals(1, list.size());
+        assertEquals(deliveryCount, list.size());
     }
 
     @Test
-    void getById() {
+    @Order(8)
+    void getById() throws Exception {
+        MvcResult result = mockMvc.perform(get(Urls.Deliveries.FULL + "/" + deliveryId1))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        DeliveryResponse response = objectMapper.readValue(content, DeliveryResponse.class);
+
+        assertNotNull(response);
+
+        DeliveryRequest deliveryRequest1 = getDelivery1(false);
+        assertEquals(deliveryRequest1.getCargo().getWeight(), response.getCargo().getWeight());
+        assertEquals(deliveryRequest1.getCargo().getDeclaredValue(), response.getCargo().getDeclaredValue());
+        assertEquals(deliveryRequest1.getCargo().getWidth(), response.getCargo().getWidth());
+        assertEquals(deliveryRequest1.getCargo().getLength(), response.getCargo().getLength());
+        assertEquals(deliveryRequest1.getCargo().getHeight(), response.getCargo().getHeight());
+
+        assertEquals(deliveryRequest1.getSender().getEmail(), response.getSender().getEmail());
+        assertEquals(deliveryRequest1.getRecipient().getEmail(), response.getRecipient().getEmail());
     }
 
     @Test
-    void getFilteredList() {
-    }
+    @Order(9)
+    void getFilteredList() throws Exception {
+        MvcResult result = mockMvc.perform(
+                get(Urls.Deliveries.Filter.FULL + "?status=PAID"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn();
 
-    @Test
-    void update() {
-    }
+        String content = result.getResponse().getContentAsString();
+        List<DeliveryResponse> response = objectMapper.readValue(content,
+                objectMapper
+                        .getTypeFactory()
+                        .constructCollectionType(List.class, DeliveryResponse.class));
 
-    @Test
-    void changeStatus() {
-    }
+        assertNotNull(response);
+        assertEquals(deliveryCountPaid, response.size());
 
-    @Test
-    void delete() {
+        if (deliveryCountPaid > 0) {
+            assertEquals(DeliveryStatus.PAID, response.get(0).getStatus());
+        }
     }
 }
