62cd6e872e174383cd19201b53258cfe1454df73
Valery Emelyanov <emelva@rambler.ru>
2021-02-18 20:08:57 +0300

    Fixing typos

--- src/main/java/com/training/core/util/RandomGenerator.java
+++ src/main/java/com/training/core/util/RandomGenerator.java
@@ -33,9 +33,9 @@ public class RandomGenerator {
         return generateRandomString(length, digits);
     }
 
-    private String generateRandomString(int lenght, String template) {
-        StringBuilder builder = new StringBuilder(lenght);
-        for (int i = 0; i < lenght; i++) {
+    private String generateRandomString(int length, String template) {
+        StringBuilder builder = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
             builder.append(template.charAt(random.nextInt(template.length())));
         }
         return builder.toString();
