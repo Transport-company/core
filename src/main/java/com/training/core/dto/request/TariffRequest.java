4b03bcc25738452acb85ebbe567f342469dc40ae
Valery Emelyanov <emelva@rambler.ru>
2021-02-26 11:16:26 +0300

    Fixing

--- src/main/java/com/training/core/dto/request/TariffRequest.java
+++ src/main/java/com/training/core/dto/request/TariffRequest.java
@@ -51,7 +51,7 @@ public class TariffRequest {
     /**
      * The minimum distance accepted for payment.
      */
-    @Schema(description = "the minimum distance accepted for payment", required = true,
+    @Schema(description = "the minimum distance (km) accepted for payment", required = true,
             example = "10")
     @NotNull(message = "The field is required")
     @Min(value = 0)
@@ -61,7 +61,7 @@ public class TariffRequest {
      * The distance from which the reduction factor is applied for the price of 1 km.
      */
     @Schema(description =
-            "the distance from which the reduction factor is applied for the price of 1 km",
+            "the distance (km) from which the reduction factor is applied for the price of 1 km",
             required = true, example = "1000")
     @NotNull(message = "The field is required")
     @Min(value = 0)
@@ -78,43 +78,59 @@ public class TariffRequest {
     /**
      * The value of the weight unit for which an additional payment is set.
      */
-    @Schema(description = "the value of the weight unit for which an additional payment is set")
+    @Schema(description =
+            "the value of the weight unit (kg) for which an additional payment is set",
+            required = true, example = "1")
+    @NotNull(message = "The field is required")
     @Min(value = 0)
-    private final float weightUnit;
+    private final Float weightUnit;
 
     /**
      * The maximum weight value above which the additional payment is set.
      */
-    @Schema(description = "the maximum weight value above which the additional payment is set")
+    @Schema(description =
+            "the maximum weight value (kg) above which the additional payment is set",
+            required = true, example = "1")
+    @NotNull(message = "The field is required")
     @Min(value = 0)
-    private final float weightThreshold;
+    private final Float weightThreshold;
 
     /**
      * Ratio increase for each additional weight unit.
      */
-    @Schema(description = "ratio increase for each additional weight unit")
+    @Schema(description = "ratio increase for each additional weight unit",
+            required = true, example = "0.1")
+    @NotNull(message = "The field is required")
     @Min(value = 0)
-    private final float weightRatioIncrease;
+    private final Float weightRatioIncrease;
 
     /**
      * The value of the volume unit for which an additional payment is set.
      */
-    @Schema(description = "the value of the volume unit for which an additional payment is set")
+    @Schema(description =
+            "the value of the volume unit (m3) for which an additional payment is set",
+            required = true, example = "0.125")
+    @NotNull(message = "The field is required")
     @Min(value = 0)
-    private final float volumeUnit;
+    private final Float volumeUnit;
 
     /**
      * The maximum volume value above which the additional payment is set.
      */
-    @Schema(description = "the maximum volume value above which the additional payment is set")
+    @Schema(description =
+            "the maximum volume value (m3) above which the additional payment is set",
+            required = true, example = "0.125")
+    @NotNull(message = "The field is required")
     @Min(value = 0)
-    private final float volumeThreashold;
+    private final Float volumeThreashold;
 
     /**
      * Ratio increase for each additional volume unit.
      */
-    @Schema(description = "ratio increase for each additional volume unit")
+    @Schema(description = "ratio increase for each additional volume unit",
+            required = true, example = "0.1")
+    @NotNull(message = "The field is required")
     @Min(value = 0)
-    private final float volumeRatioIncrease;
+    private final Float volumeRatioIncrease;
 
 }
